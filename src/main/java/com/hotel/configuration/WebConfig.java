33ff112e641614e45503fa0aab332fe812445942
ctrianaCorporasioncic <ctriana@corrosioncic.com>
2024-11-29 17:32:26 -0500

    new try of allow cors

--- src/main/java/com/hotel/configuration/WebConfig.java
+++ src/main/java/com/hotel/configuration/WebConfig.java
@@ -6,12 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // Permitir todos los endpoints
-                .allowedOrigins("https://hotel-la-sierra.netlify.app")  // Reemplaza con la URL de tu frontend en Netlify
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        // Configura CORS para permitir solicitudes desde el dominio de tu frontend desplegado
+        registry.addMapping("/**") // Permitir todas las rutas
+                .allowedOrigins("https://hotel-la-sierra.netlify.app") // El dominio de tu frontend desplegado
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos los encabezados
+                .allowCredentials(true); // Permite el envío de cookies o credenciales, si es necesario
     }
 }
