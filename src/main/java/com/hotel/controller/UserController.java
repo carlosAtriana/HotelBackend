746a44e7e32ea969efab836d828c63092b225be0
ctrianaCorporasioncic <ctriana@corrosioncic.com>
2024-11-29 18:11:57 -0500

    version compiled3

--- src/main/java/com/hotel/controller/UserController.java
+++ src/main/java/com/hotel/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/usuarios")
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class UserController {
 
